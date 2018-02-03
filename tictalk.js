4e5f7ec7b1d6b7bd87b97d3a9940d1369c77e154
Darius Cepulis <de.cepulis@gmail.com>
2018-02-03 18:07:21 -0500

    Added Calendar and Buttons

--- tictalk.js
+++ tictalk.js
@@ -1,4 +1,20 @@
 // When the document loads, we execute this code
 $(document).ready(function(){ 
-	console.log("Document Loaded")
+
+	$('#calendar').fullCalendar({
+		navLinks: true, // can click day/week names to navigate views
+      	editable: true,
+      	droppable: true,
+      	
+      	height: ($(window).height()-80),
+
+        dayClick: function() {
+	        alert('a day has been clicked!');
+	    },
+
+	    header: {
+	        left:'title',
+	        right: 'agendaDay,agendaWeek,month, prev,next'
+	     }
+    })
 })
