a730fc1a4aa332f2844c1aa045c4dbdac4673ed9
Stefan Apostoluk <sapostoluk@users.noreply.github.com>
2018-03-14 00:32:48 -0400

    Improved sign-in handling and basic profile info fetching
    Updated and tuned-up the authentication handling as well as added basic
    Google profile info (name, email, and image) to the account section.

--- googleauth.js
+++ googleauth.js
@@ -7,15 +7,25 @@ var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/
 
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
+// @todo - Need to add writing scope eventually
 var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
 
-var authorizeButton = document.getElementById('authorize-button');
-var signoutButton = document.getElementById('signout-button');
+// Var containing info about the Google auth instance
+// Used to call gapi.auth2.GoogleAuth's methods
+// @todo - Might be bad to only do this once? I don't think so, but we'll find that out for sure later.
+var googleAuth;
+
+// Var containing info about the Google user of the Google auth instance
+// Used to call gapi.auth2.GoogleAuth.currentUser.get()'s (aka CurrentUser) methods
+var googleUser;
+
+// Var containing info about the basic profile info of the Google user
+var basicProfile;
 
 /**
  *  On load, called to load the auth2 library and API client library.
  */
- function handleClientLoad() {
+function handleClientLoad() {
   gapi.load('client:auth2', initClient);
 }
 
@@ -23,95 +33,89 @@ var signoutButton = document.getElementById('signout-button');
  *  Initializes the API client library and sets up sign-in state
  *  listeners.
  */
- function initClient() {
+function initClient() {
   gapi.client.init({
     apiKey: API_KEY,
     clientId: CLIENT_ID,
     discoveryDocs: DISCOVERY_DOCS,
     scope: SCOPES
   }).then(function () {
+    // Getting information about the Google auth instance and defining the GoogleAuth var
+    googleAuth = gapi.auth2.getAuthInstance();
     // Listen for sign-in state changes.
-    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+    googleAuth.isSignedIn.listen(updateSigninStatus);
 
     // Handle the initial sign-in state.
-    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    authorizeButton.onclick = handleAuthClick;
-    signoutButton.onclick = handleSignoutClick;
+    // This pretty little line gets us the sign in-state
+    updateSigninStatus(googleAuth.isSignedIn.get());
   });
 }
 
 /**
  *  Called when the signed in status changes, to update the UI
  *  appropriately. After a sign-in, the API is called.
+ * 
+ *  @param {boolean} isSignedIn - Whether or not the user is signed in yet
  */
- function updateSigninStatus(isSignedIn) {
-  if (isSignedIn) {
-    authorizeButton.style.display = 'none';
-    signoutButton.style.display = 'block';
+function updateSigninStatus(isSignedIn) {
+  if (isSignedIn) {    // If the user is signed in:
+    console.log("DEBUG: User signed in.")
+    // 1) Display user info in the user info section
+
+    // 2) Display the sign-out button when you click on the user info
+    $('#signout-button').click(handleSignoutClick);
+
+    // 3) Get basic user info
+    googleUser = googleAuth.currentUser.get();
+    basicProfile = googleUser.getBasicProfile();
+    console.log("DEBUG: Basic profile acquired")
+
+    // 4) Display basic profile info
+    $("#bp-image").attr("src", basicProfile.getImageUrl());
+    $("#bp-name").text(basicProfile.getName());
+    $("#bp-email").text(basicProfile.getEmail());
+
+    // Legacy code below
     listUpcomingEvents();
-  } else {
-    authorizeButton.style.display = 'block';
-    signoutButton.style.display = 'none';
+  } else {    // If the user isn't signed in:
+    console.log("DEBUG: User not signed in.")
+
+    // 1) Bring up a login view 
+    $('#authenticate-popup').addClass('active')
+    // 2) Handle the sign in button
+    $('#signin-button').click(handleSignInClick);
   }
+} 
+
+/**
+ * Called when the current user changes 
+ *
+ * @param currentUser - The current Google user of the authenticated Google auth instance
+ */
+function updateCurrentUser(currentUser) {
+
 }
+
+// ---------------
+// Button Handlers
+// ---------------
 
 /**
  *  Sign in the user upon button click.
  */
- function handleAuthClick(event) {
-  gapi.auth2.getAuthInstance().signIn();
+function handleSignInClick(event) {
+  googleAuth.signIn();
 }
 
 /**
  *  Sign out the user upon button click.
  */
- function handleSignoutClick(event) {
-  gapi.auth2.getAuthInstance().signOut();
+function handleSignoutClick(event) {
+  googleAuth.signOut();
 }
 
 
-// EVERYTHING BELOW THIS LINE CAN BE MOVED TO A DIFFERENT FILE 
+// --------------------------------
+// Test Methods and Helper Methods:
+// --------------------------------
 
-/**
- * Append a pre element to the body containing the given message
- * as its text node. Used to display the results of the API call.
- *
- * @param {string} message Text to be placed in pre element.
- */
- function appendPre(message) {
-  var pre = document.getElementById('content');
-  var textContent = document.createTextNode(message + '\n');
-  pre.appendChild(textContent);
-}
-
-/**
- * Print the summary and start datetime/date of the next ten events in
- * the authorized user's calendar. If no events are found an
- * appropriate message is printed.
- */
- function listUpcomingEvents() {
-  gapi.client.calendar.events.list({
-    'calendarId': 'primary',
-    'timeMin': (new Date()).toISOString(),
-    'showDeleted': false,
-    'singleEvents': true,
-    'maxResults': 10,
-    'orderBy': 'startTime'
-  }).then(function(response) {
-    var events = response.result.items;
-    appendPre('Upcoming events:');
-
-    if (events.length > 0) {
-      for (i = 0; i < events.length; i++) {
-        var event = events[i];
-        var when = event.start.dateTime;
-        if (!when) {
-          when = event.start.date;
-        }
-        appendPre(event.summary + ' (' + when + ')')
-      }
-    } else {
-      appendPre('No upcoming events found.');
-    }
-  });
-}
