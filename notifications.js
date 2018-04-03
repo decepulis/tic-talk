b1ec2d4e9aa97a8415f3bf3f7ea0ffd65d1387ee
decepulis <de.cepulis@gmail.com>
2018-04-02 21:52:42 -0400

    Notification Mockups

---
+++ notifications.js
@@ -0,0 +1,40 @@
+// request permission on page load
+$(document).ready(function(){
+  document.addEventListener('DOMContentLoaded', function () {
+  if (!Notification) {
+    alert('Desktop notifications not available in your browser. Try Chromium.'); 
+    return;
+  }
+
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+});})
+
+function notify(title,body,url) {
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+  else {
+    var notification = new Notification(title, {
+      icon: './favicons/_src/TTmedium.png',
+      body: body,
+    });
+
+    notification.onclick = function () {
+      window.open(url);      
+    };
+  }
+}
+
+function scheduleWeek() {
+  var title = "Tic Talk: Schedule your Week!";
+  var body  = "Spend 5 minutes planning your week.";
+  var url   = "http://localhost:8000/Library/Mobile%20Documents/com~apple~CloudDocs/SS18/Senior%20Design%202/tic-talk/index.html";
+  notify(title,body,url)
+}
+
+function reflectDay() {
+  var title = "Tic Talk: Reflect on your Day!";
+  var body  = "How long did your work take today?";
+  var url   = "http://localhost:8000/Library/Mobile%20Documents/com~apple~CloudDocs/SS18/Senior%20Design%202/tic-talk/index.html";
+  notify(title,body,url)
+}
